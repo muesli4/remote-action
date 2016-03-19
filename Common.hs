d8f80aba1a5c85a435c92376f230277b877173be
Moritz Bruder <muesli4@gmail.com>
2016-03-19 16:26:09 +0100

    Added simple error messages, from IniConfig.

--- Common.hs
+++ Common.hs
@@ -13,13 +13,16 @@ progName = "remote-action"
 withJust :: Maybe a -> b -> (a -> b) -> b
 withJust m def f = maybe def f m
 
+withEither :: Either a b -> (a -> c) -> (b -> c) -> c
+withEither e l r = either l r e
+
 withConfigFile :: FilePath -> (Config -> IO a) -> IO a
 withConfigFile fp f = do
     exists <- doesFileExist fp
     if exists
     then do
-        mConf <- readConfigFile fp
-        withJust mConf failSyntaxError f
+        eConf <- readConfigFile fp
+        withEither eConf failConfig f
     else failConfig "file not found"
 
 -- | Tries to find a config file in the local path and then in the paths given
@@ -27,13 +30,10 @@ withConfigFile fp f = do
 withXDGConfigFile :: FilePath -> (Config -> IO a) -> IO a
 withXDGConfigFile fn f = do
     xdgPaths <- getAllConfigFiles progName fn
-    mConf <- do
+    eConf <- do
         mFp <- firstM doesFileExist $ fn : xdgPaths
         withJust mFp (failConfig "no config file found") $ readConfigFile
-    withJust mConf failSyntaxError f
-
-failSyntaxError :: IO a
-failSyntaxError = failConfig "syntax error"
+    withEither eConf failConfig f
 
 failConfig :: String -> IO a
 failConfig msg = die $ "Failed to read configuration file: " ++ msg
